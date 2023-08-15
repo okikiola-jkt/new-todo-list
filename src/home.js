5eb0ab21506725f4c0b6bc63eb02600fe78c4c77
Okikiola Ojikutu <okikiolaojikutue@gmail.com>
2023-08-15 14:18:29 -0600

    css

--- src/home.js
+++ src/home.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Home = () => {
     return (
         <div className="home-page">
@@ -5,20 +7,20 @@ const Home = () => {
                 <h1>My Todo List</h1>
             </header>
             <form action="">
-            <input type="text" className="todo-input" />
-            <button className="todo-button" type="button">
-            <i class="fas fa-plus-circle fa-lg"></i>
-            </button>
-            <select name="todo" className="todo-select">
-            <option value="All">All</option>
-            <option value="Completed">Completed</option>
-            <option value="Pending">Pending</option>
-            </select>
+                <input type="text" className="todo-input" />
+                <button className="todo-button" type="button">
+                    <i class="fas fa-plus-circle fa-lg"></i>
+                </button>
+                    <select name="todo" className="todo-select">
+                        <option value="All">All</option>
+                        <option value="Completed">Completed</option>
+                        <option value="Pending">Pending</option>
+                    </select>
             </form>
-
             <div className="todo-container">
                 <ul className="todo-list"></ul>
             </div>
+            
         </div>
         
       );
